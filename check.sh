ad56ec7772230b1b8d987e7f360e1930eaeced2e
flutter <adhammohamed@Adhams-MacBook-Pro.local>
2023-12-18 01:38:43 +0200

    editing

--- check.sh
+++ check.sh
@@ -3,16 +3,17 @@
 function is_valid_email() {                 # Function to check if an email is a valid or not
     local email=$1
     if [[ $email =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then       #REGEX For Email 
-        return 0
+        echo "Valid Email"
     else
-        return 1
+        echo "invalid Email"
     fi
 }
 
 
 function odd_or_even() {                    # Function to determine if a number is Even or Odd
     local num=$1
-    if (( num % 2 == 0 )); then
+    if (( $num % 2 == 0 ))
+     then
         echo "even"
     else
         echo "odd"
@@ -20,10 +21,13 @@ function odd_or_even() {                    # Function to determine if a number 
 }
 
 # Process the text file
-while read -r name email id; do
-    if [[ -n $id && $id =~ ^[0-9]+$ ]]; then
-        if is_valid_email "$email"; then
-            echo "The ID of $email is $(odd_or_even $id) number."
-        fi
+while read -r name email id 
+do
+    if [[ -n $id && $id =~ ^[0-9]+$ ]]
+     then
+    if is_valid_email "$email"
+    then
+    echo "The ID of $email is $(odd_or_even $id) number."
+    fi
     fi
 done
